e279b63057d60c8ca1d4de0fd7e5997a1641ee63
DaboRestrepo <daborestrepo@gmail.com>
2021-12-21 15:23:41 -0500

    Fix negative output

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-mod = number % 10
-if mod == 0:
+mod = abs(number) % 10
+if number < 0:
+    mod = -mod
+    print('Last digit of', number, 'is', mod, 'and is less than 6 and not 0')
+elif mod == 0:
     print('Last digit of', number, 'is', mod, 'and is 0')
 elif mod > 5:
     print('Last digit of', number, 'is', mod, 'and is greater than 5')
-elif number < 6:
-    if number < 0:
-        abs = number * -1
-        mod = abs % 10
+else:
     print('Last digit of', number, 'is', mod, 'and is less than 6 and not 0')
