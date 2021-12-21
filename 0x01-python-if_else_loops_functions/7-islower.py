859a84e1b8b094d765abd7d4e5e2352575870bf6
DaboRestrepo <daborestrepo@gmail.com>
2021-12-21 18:19:32 -0500

    Is lowercase

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
-	if ord(c) in range(65, 91):
-		return (False)
-	elif ord(c) in range (97, 123):
+	if ord(c) in range(97, 123):
 		return (True)
+	#elif ord(c) in range (97, 123):
+	else:
+		return (False)
