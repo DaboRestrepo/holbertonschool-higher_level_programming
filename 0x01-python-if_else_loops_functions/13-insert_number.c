62944e0c968398412389a8fe3e38b19f3d833566
DaboRestrepo <daborestrepo@gmail.com>
2021-12-21 20:22:20 -0500

    Insert a node in the linked list

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * insert_node - Insert a node in a place of the list
+ * @head: list
+ * @number: number to add
+ * Return: the new list
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new = NULL, *h = *head;
+	int idx = 0;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->n = number;
+	for (; idx < 4; idx++)
+	{
+		h = h->next;
+	}
+	new->next = h->next;
+	h->next = new;
+	return (new);
+}
