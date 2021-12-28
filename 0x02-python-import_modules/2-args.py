8ae54e29d7c17c1b4e975a2f7d792489ace90c7c
DaboRestrepo <daborestrepo@gmail.com>
2021-12-28 18:02:51 -0500

    Fix the print

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-from typing import Counter
-
-
 if __name__ == "__main__":
     import sys
     argv = sys.argv
-    count = 0
-    for i in range(len(argv)):
-        count = count + 1
-    print('{} arguments.'.format(count - 1), end='\n')
+    count = len(argv)
+    if (count - 1) == 0:
+        print('{} arguments.'.format(count - 1), end='\n')
+    else:
+        print('{} arguments:'.format(count - 1), end='\n')
     for i in range(len(argv)):
         if i >= 1:
             print('{}: {}'.format(i, argv[i], end=''))
