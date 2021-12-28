06a560f1635b4eab855578da3365b29978d28a05
DaboRestrepo <daborestrepo@gmail.com>
2021-12-28 18:05:35 -0500

    fix the singular

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,8 @@ if __name__ == "__main__":
     count = len(argv)
     if (count - 1) == 0:
         print('{} arguments.'.format(count - 1), end='\n')
+    elif (count - 1) == 1:
+        print('{} argument:'.format(count - 1), end='\n')
     else:
         print('{} arguments:'.format(count - 1), end='\n')
     for i in range(len(argv)):
