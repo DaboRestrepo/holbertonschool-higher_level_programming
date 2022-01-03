bf20aa4450fd6eeb2fa81d843297be91bf3c5cff
DaboRestrepo <daborestrepo@gmail.com>
2022-01-03 12:31:42 -0500

    Fix the condition or

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if my_list[idx] > 0 | my_list[idx] < int(len(my_list)):
-        return my_list[idx]
-    else:
+    if my_list[idx] < 0 or my_list[idx] > int(len(my_list)):
         return None
+    return my_list[idx]
