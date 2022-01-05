7f1a2f0bd1627a14c401de917a5c3e2fd757e903
DaboRestrepo <daborestrepo@gmail.com>
2022-01-05 14:03:52 -0500

    Fix the range

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     if matrix is not None:
-        square = [[(row[item] ** 2) for item in range(3)] for row in matrix]
+        square = [[item ** 2 for item in row] for row in matrix]
         return square
