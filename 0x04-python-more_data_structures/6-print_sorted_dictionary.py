ed3b136ad42f1e35cc6ea3f7e663de6d06d88529
DaboRestrepo <daborestrepo@gmail.com>
2022-01-05 17:12:02 -0500

    Fix the value

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    if a_dictionary is not None:
-        for key, value in sorted(a_dictionary.items()):
-            print('{0} : {1}'.format(key, a_dictionary[key]))
+    for key, value in sorted(a_dictionary.items()):
+        print('{0} : {1}'.format(key, value))
