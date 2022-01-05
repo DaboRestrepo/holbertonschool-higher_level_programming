41e02e987ad2eefa273fb260056de355fb630fc2
DaboRestrepo <daborestrepo@gmail.com>
2022-01-05 18:40:03 -0500

    fix the conditions

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,14 @@
 #!/usr/bin/pytho3
 def roman_to_int(roman_string):
     dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    roman_list = list(roman_string)
+    # roman_list = list(roman_string)
     add = 0
-    if type(roman_string) is not str or len(roman_string) == 0:
+    roman = roman_string
+    if type(roman) is not str or len(roman) == 0:
         return 0
-    for i in roman_list:
-        for j in dict:
-            if i == j:
-                add += dict[j]
+    for i in range(len(roman)):
+        if i < len(roman) - 1 and dict[roman[i]] < dict[roman[i + 1]]:
+            add -= dict[roman[i]]
+        else:
+            add += dict[roman[i]]
     return add
