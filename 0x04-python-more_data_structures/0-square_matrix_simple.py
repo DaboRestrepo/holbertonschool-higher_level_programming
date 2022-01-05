2a61f5c4e64b5c18f7edd7c8c8d58a3c12b03d0c
DaboRestrepo <daborestrepo@gmail.com>
2022-01-05 13:50:06 -0500

    Fix the conditional

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    square = [[(row[item] ** 2) for item in range(3)] for row in matrix]
-    return square
+    if matrix is not None:
+        square = [[(row[item] ** 2) for item in range(3)] for row in matrix]
+        return square
