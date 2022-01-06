02104180bd1935ba8ba560eda7de7b26ab4d57c2
DaboRestrepo <daborestrepo@gmail.com>
2022-01-05 19:20:06 -0500

    Fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,10 +1,8 @@
 #!/usr/bin/pytho3
 def roman_to_int(roman_string):
-    if type(roman_string) is not str or roman_string is None:
-        return 0
-    if roman_string.islower():
-        return 0
-    else:
+    if roman_string is not None:
+        if type(roman_string) != str or roman_string is None:
+            return 0
         dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
                 'D': 500, 'M': 1000}
         add = 0
