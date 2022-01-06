14b3df034811b580956e6631c068dedcf989a313
DaboRestrepo <daborestrepo@gmail.com>
2022-01-05 19:24:00 -0500

    Fix if

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,4 +1,4 @@
-#!/usr/bin/pytho3
+#!/usr/bin/python3
 def roman_to_int(roman_string):
     if roman_string is not None and type(roman_string) == str:
         dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
