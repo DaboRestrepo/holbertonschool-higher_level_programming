fa0fb86e6049072a70bb6359c327a78ea94133d6
DaboRestrepo <daborestrepo@gmail.com>
2022-01-17 13:54:14 -0500

    Add typeError

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,8 +2,7 @@
 def safe_print_integer(value):
     if value is not None:
         try:
-            value = int(value)
             print("{:d}".format(value))
             return True
-        except ValueError:
+        except (ValueError, TypeError):
             return False
