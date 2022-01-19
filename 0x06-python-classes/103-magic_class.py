5455d71159d9a4ae5ea09521cd58c9216da1ef5e
DaboRestrepo <daborestrepo@gmail.com>
2022-01-19 15:59:04 -0500

    Python Bytecode

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Class to know the area and circumference"""
+from math import pi
+
+
+class MagicClass:
+    """Class to define the area and the circumference"""
+    def __init__(self, radius=0):
+        """Inicialize the radius variable"""
+        self.__radius = 0
+        if type(radius) is not int or type(radius) is not float:
+            raise TypeError('radius must be a number')
+        self.__radius = radius
+
+    def area(self, math):
+        """Calculate the area importing pi from math"""
+        return (self.__radius ** 2) * math.pi
+
+    def circumference(self, math):
+        """Calculate the circumference importing pi from math"""
+        return 2 * math.pi * self.__radius
