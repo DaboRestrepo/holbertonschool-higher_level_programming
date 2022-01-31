52e99e2a1d753e67cd002999d5c78b75e134470a
DaboRestrepo <daborestrepo@gmail.com>
2022-01-31 18:25:43 -0500

    Fix the doc

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""This class sort and print a giving list
-This supplies the print_sorted function"""
+"""This class sort and print a giving list."""
 
 
 class MyList(list):
-
+    """Supplies the function print_sorted"""
     def print_sorted(self):
         """This function sort and print a list"""
         print(sorted(self))
