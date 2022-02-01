0cf251cabd3f9b7154215a3e0c69767f806bc967
DaboRestrepo <daborestrepo@gmail.com>
2022-01-31 19:20:30 -0500

    Raise an exception error

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Empty class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Empty class BaseGeometry"""
+    def area(self):
+        """Function to raise and Exception error"""
+        raise Exception("area() is not implemented")
