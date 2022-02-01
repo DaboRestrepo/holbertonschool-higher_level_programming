05e091abb25364c1eb80cd9996ed5407c5258f6f
DaboRestrepo <daborestrepo@gmail.com>
2022-01-31 19:05:09 -0500

    Inherites from

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,4 +6,5 @@ def inherits_from(obj, a_class):
     """Determine if the object inherites of the class"""
     if isinstance(obj, a_class) and type(obj) is a_class:
         return False
-    return True
+    else:
+        return True
