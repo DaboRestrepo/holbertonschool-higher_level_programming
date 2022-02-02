e0c820d98d9d82c2f85dc95ed545305e52275adf
DaboRestrepo <daborestrepo@gmail.com>
2022-02-01 19:57:24 -0500

    Make a list with the arguments

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from sys import argv
+import json
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+list = []
+file = 'add_item.json'
+try:
+    list = load_from_json_file(file)
+except BaseException:
+    pass
+for i in range(1, len(argv)):
+    list.append(argv[i])
+save_to_json_file(list, file)
