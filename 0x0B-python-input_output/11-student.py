a20736652acd74fcedf283d609aafa4397406151
DaboRestrepo <daborestrepo@gmail.com>
2022-02-02 14:24:15 -0500

    Reload the dictionary

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,24 +1,8 @@
 #!/usr/bin/python3
 """This module supplies the class Student"""
+Student = __import__('10-student').Student
 
 
-class Student:
-    """In this class is the to_json function"""
-    def __init__(self, first_name, last_name, age):
-        """Instantiation"""
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
-
-    def to_json(self, attrs=None):
-        """Return the data in dictionary way"""
-        if type(attrs) is list:
-            data = {"first_name": self.first_name,
-                    "last_name": self.last_name, "age": self.age}
-            return {element: data[element] for element in attrs if
-                    type(element) is str and element in data.keys()}
-        return self.__dict__
-
-    def reload_from_json(self, json):
-        """Reload the json dictionary"""
-        self.__dict__ = json
+def reload_from_json(self, json):
+    """Reload the json dictionary"""
+    self.__dict__ = json
