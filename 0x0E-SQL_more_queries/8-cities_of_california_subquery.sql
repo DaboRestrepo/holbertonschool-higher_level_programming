f664456e27316133c14bbad5b2b845cd4946c041
DaboRestrepo <daborestrepo@gmail.com>
2022-03-16 16:57:36 -0500

    Change state_id by id

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,8 @@
 -- list the cities of California.
 SELECT id, name
 FROM cities
-WHERE state_id=(
-    SELECT state_id
+WHERE state_id IN 
+    (SELECT id
     FROM states
     WHERE name = 'California') 
 ORDER BY id;
