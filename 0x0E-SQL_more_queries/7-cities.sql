51b2096bc78cec064629aaedb949b593dc5207ef
DaboRestrepo <daborestrepo@gmail.com>
2022-03-16 16:17:04 -0500

    Taking off the quotes

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,5 +5,5 @@ CREATE TABLE IF NOT EXISTS cities (
     id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    CONSTRAINT 'cities_ibfk_1' FOREIGN KEY (id) REFERENCES states(id)
+    CONSTRAINT cities_ibfk_1 FOREIGN KEY (id) REFERENCES states(id)
 ) ENGINE=InnoDB;
