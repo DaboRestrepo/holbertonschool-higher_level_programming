10b1e47867bc053d693336d10d4766a8b5691f71
DaboRestrepo <daborestrepo@gmail.com>
2022-05-09 18:14:11 -0500

    Filter in a safe way

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Get a states list from the database"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    """Has to take 3 arguments:
+    argv[1]: username
+    argv[2]: password
+    argv[3]: database name
+    argv[4]: user input.
+    It must filter a state using an input in safe way."""
+    connection = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                                 passwd=argv[2], db=argv[3])
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM states\
+                   WHERE name = %(name)s\
+                   ORDER BY id;", {'name': argv[4]})
+    results = cursor.fetchall()
+    for data in results:
+        print(data)
+    cursor.close()
+    connection.close()
