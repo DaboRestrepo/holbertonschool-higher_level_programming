854beacc617471b7441f2f65fbb55aa733dbd1b1
DaboRestrepo <daborestrepo@gmail.com>
2022-05-09 21:22:40 -0500

    Print the first state

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""List the states."""
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import (create_engine)
+
+
+if __name__ == "__main__":
+    """List the states by id, using SQLAlchemy."""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    result = engine.execute("SELECT states.id, states.name\
+                            FROM states\
+                            ORDER BY states.id\
+                            LIMIT 1;")
+    for data in result:
+        print("{}: {}".format(data.id, data.name))
