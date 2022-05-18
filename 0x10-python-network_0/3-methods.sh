98a1ea4413faae79131c384bc5bdbc54debc5a3c
DaboRestrepo <daborestrepo@gmail.com>
2022-05-18 18:51:44 -0500

    fix the output line

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Display all the HTTP methods the server will accept.
-curl -siX OPTIONS "$1" | awk '/Allow/{print $2, $3, $4}'
+curl -siX OPTIONS "$1" | awk '/Allow/{print substr($0, 8)}'
