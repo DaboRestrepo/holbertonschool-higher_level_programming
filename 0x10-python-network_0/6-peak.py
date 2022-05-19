d8605686219e647853be3563499266cffbbc3657
DaboRestrepo <daborestrepo@gmail.com>
2022-05-18 20:35:26 -0500

    reduce the function

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,5 +6,4 @@ def find_peak(list_of_integers):
     """Finds a peak in a list of integers.
     list_of_integers: Unsorted int list."""
     if list_of_integers:
-        list = sorted(list_of_integers)
-        return list[-1]
+        return sorted(list_of_integers)[-1]
