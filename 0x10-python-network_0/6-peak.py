fa1db299f157518eaf0ef27fbf529c6cdbdb95d6
DaboRestrepo <daborestrepo@gmail.com>
2022-05-18 20:22:26 -0500

    Find a peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module soport the find_peak function"""
+
+
+def find_peak(list_of_integers):
+    """Finds a peak in a list of integers.
+    list_of_integers: Unsorted int list."""
+    if list_of_integers:
+        list = sorted(list_of_integers)
+        return list[-1]
+    else:
+        return None
