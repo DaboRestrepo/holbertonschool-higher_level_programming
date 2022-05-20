db0bc2bf3fd50d2cd35386759f5cee7172af9936
DaboRestrepo <daborestrepo@gmail.com>
2022-05-20 09:45:34 -0500

    Fix the print format and add the if name condition

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,8 +3,10 @@
 from email.headerregistry import ContentTypeHeader
 import urllib.request
 
-
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
-    res_read = res.read()
-    print('Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}'
-          .format(type(res_read), res_read, res.reason))
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
+        res_read = res.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(res_read)))
+        print('\t- content: {}'.format(res_read))
+        print('\t- utf8 content: {}'.format(res.reason))
