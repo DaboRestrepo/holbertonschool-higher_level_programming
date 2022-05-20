7cf7cd5d0d0dd17792ab9719ec7b1b90e1d74ed0
DaboRestrepo <daborestrepo@gmail.com>
2022-05-20 11:16:39 -0500

    change the encoding

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,15 +5,9 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    url_split = argv[1].split('/')
-    if "post_email" in url_split:
-        url = argv[1]
-    else:
-        url = argv[1] + "/post_email"
-    values = {'email': argv[2]}
-    data = parse.urlencode(values)
-    data = data.encode('ascii')
-    r = request.Request(url, data)
-    with request.urlopen(r) as res:
+    url = argv[1]
+    values = {"email": argv[2]}
+    data = parse.urlencode(values).encode('utf-8')
+    with request.urlopen(url, data) as res:
         page = res.read().decode('utf-8')
         print(page)
