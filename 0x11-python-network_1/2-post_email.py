fdeec82f8c8ca07995f1a2cf106e65a93e6d9bb9
DaboRestrepo <daborestrepo@gmail.com>
2022-05-20 11:05:33 -0500

    Post an email to the URL request

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,11 +5,15 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    url = argv[1]
+    url_split = argv[1].split('/')
+    if "post_email" in url_split:
+        url = argv[1]
+    else:
+        url = argv[1] + "/post_email"
     values = {'email': argv[2]}
-
     data = parse.urlencode(values)
     data = data.encode('ascii')
     r = request.Request(url, data)
     with request.urlopen(r) as res:
         page = res.read().decode('utf-8')
+        print(page)
