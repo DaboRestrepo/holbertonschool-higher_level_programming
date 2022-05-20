9a72e54630db0037893139eb119093e896c3bd3e
DaboRestrepo <daborestrepo@gmail.com>
2022-05-20 10:02:48 -0500

    Fix the decode part

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """Fetch an URL with urllib."""
-import urllib.request
+from urllib import request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
+    with request.urlopen('http://0.0.0.0:5050/status') as res:
         res_read = res.read()
         print('Body response:')
         print('\t- type: {}'.format(type(res_read)))
         print('\t- content: {}'.format(res_read))
-        print('\t- utf8 content: {}'.format(res.reason))
+        print('\t- utf8 content: {}'.format(res_read.decode('utf-8')))
